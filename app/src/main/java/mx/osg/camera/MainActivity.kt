88345786b1d7170a03645b95a6d0381d4a8140a1
ArcOscar <oscarperezgutierrez77@gmail.com>
2020-11-28 23:52:37 -0600

    Mejor funcionamiento del guardado de la foto por medio de botón. Mejor vista para el usuario. Arreglo del guardado de las fotos.

--- app/src/main/java/mx/osg/camera/MainActivity.kt
+++ app/src/main/java/mx/osg/camera/MainActivity.kt
@@ -2,23 +2,19 @@ package mx.osg.camera
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.widget.ImageView
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.FileOutputStream
 
-
-private const val FILE_NAME = "photo.jpg"
 private const val REQUEST_CODE = 42
 private lateinit var photoFile: File
 
@@ -26,13 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     // Check if need to ask permissions
     // Lollipop and before versions will not require permissions
-    protected fun shouldAskPermissions(): Boolean {
+    private fun shouldAskPermissions(): Boolean {
         return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
     }
 
     // Ask for the permissions
     @RequiresApi(Build.VERSION_CODES.M)
-    protected fun askPermissions() {
+    private fun askPermissions() {
         val permissions = arrayOf(
             "android.permission.READ_EXTERNAL_STORAGE",
             "android.permission.WRITE_EXTERNAL_STORAGE"
@@ -45,45 +41,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Taking the picture to use in the app
+        // Clicking the button for the camera
         btnTakePicture.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile = getPhotoFile(FILE_NAME)
-
-            val fileProvider = FileProvider.getUriForFile(
-                this,
-                "mx.osg.camera.fileprovider",
-                photoFile
-            )
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_CODE)
-            } else {
-                Toast.makeText(this, "No es posible abrir la cámara", Toast.LENGTH_SHORT).show()
-            }
+            takingPhoto()
         }
     }
 
-    private fun getPhotoFile(fileName: String): File {
+    // Opening camera and taking the photo
+    private fun takingPhoto() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = getPhotoFile(System.currentTimeMillis().toString())
+
+        val fileProvider = FileProvider.getUriForFile(
+            this,
+            "mx.osg.camera.fileprovider",
+            photoFile
+        )
+
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+        if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_CODE)
+        } else {
+            Toast.makeText(this, "No es posible abrir la cámara", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getPhotoFile(fName: String): File {
         // Use `getExternalFilesDir` on Context to access package-specific directories.
         val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(fileName, ".jpg", storageDirectory)
+        return File.createTempFile(fName, ".jpg", storageDirectory)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
-            imageView.setImageBitmap(takenImage)
+            val imageTaken = BitmapFactory.decodeFile(photoFile.absolutePath)
+            imageView.setImageBitmap(imageTaken)
 
-            // Check if the app has the required permissions. If not granted, will ask for them
-            if (shouldAskPermissions()) {
-                askPermissions()
+            btnSavePicture.visibility = View.VISIBLE
+            btnSavePicture.setOnClickListener {
+                // Check if the app has the required permissions. If not granted, will ask for them
+                if (shouldAskPermissions())
+                    askPermissions()
                 saveToGallery()
             }
-            else {
-                saveToGallery()
-            }
-
         } else {
             super.onActivityResult(requestCode, resultCode, intent)
         }
@@ -91,12 +91,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Saving the photo into the gallery
-    fun saveToGallery() {
-        val image: ImageView = findViewById(R.id.imageView)
-        image.buildDrawingCache()
-        val bm = image.drawingCache
-        MediaStore.Images.Media.insertImage(getContentResolver(), bm,
-            System.currentTimeMillis().toString() + ".jpg" , "Pruebas de Proyecto Final");
+    private fun saveToGallery() {
+        imageView.destroyDrawingCache()
+        imageView.buildDrawingCache()
+        val bm = imageView.drawingCache
+        MediaStore.Images.Media.insertImage(
+            contentResolver, bm,
+            System.currentTimeMillis().toString() + ".jpg" , "Pruebas de Proyecto Final")
         Toast.makeText(applicationContext, "Imagen guardada en galería", Toast.LENGTH_LONG).show()
     }
 }
