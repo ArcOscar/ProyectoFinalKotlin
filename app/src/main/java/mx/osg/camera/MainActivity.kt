21b420a49ea1b3d2eb9240dc83b90444c449ff28
ArcOscar <oscarperezgutierrez77@gmail.com>
2020-12-13 16:34:26 -0600

    Filtro negativo agregado y arreglo de la imagen presentada al usuario.

--- app/src/main/java/mx/osg/camera/MainActivity.kt
+++ app/src/main/java/mx/osg/camera/MainActivity.kt
@@ -2,7 +2,9 @@ package mx.osg.camera
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -14,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+
 
 private const val REQUEST_CODE = 42
 private lateinit var photoFile: File
@@ -74,10 +77,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val imageTaken = BitmapFactory.decodeFile(photoFile.absolutePath)
+
+            val imageTaken = RotateBitmap(BitmapFactory.decodeFile(photoFile.absolutePath))
             imageView.setImageBitmap(imageTaken)
 
             btnSavePicture.visibility = View.VISIBLE
+            btnNegative.visibility = View.VISIBLE
+
+            btnNegative.setOnClickListener {
+                try { imageView.setImageBitmap(negative(imageTaken)) }
+                catch (e: Exception) {}
+            }
+
             btnSavePicture.setOnClickListener {
                 // Check if the app has the required permissions. If not granted, will ask for them
                 if (shouldAskPermissions())
@@ -90,6 +101,34 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun RotateBitmap(source: Bitmap): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(90F)
+        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
+    }
+
+    // Apply negative effect on the bitmap
+    fun negative(original: Bitmap): Bitmap? {
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val neg = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        // Get info about Bitmap
+        val width = neg.width
+        val height = neg.height
+        val pixels = width * height
+
+        // Get original pixels
+        val pixel = IntArray(pixels)
+        neg.getPixels(pixel, 0, width, 0, 0, width, height)
+
+        // Modify pixels
+        for (i in 0 until pixels) pixel[i] = pixel[i] xor 0x00FFFFFF
+        neg.setPixels(pixel, 0, width, 0, 0, width, height)
+
+        // Return inverted Bitmap
+        return neg
+    }
+
     // Saving the photo into the gallery
     private fun saveToGallery() {
         imageView.destroyDrawingCache()
@@ -97,7 +136,8 @@ class MainActivity : AppCompatActivity() {
         val bm = imageView.drawingCache
         MediaStore.Images.Media.insertImage(
             contentResolver, bm,
-            System.currentTimeMillis().toString() + ".jpg" , "Pruebas de Proyecto Final")
+            System.currentTimeMillis().toString() + ".jpg", "Pruebas de Proyecto Final"
+        )
         Toast.makeText(applicationContext, "Imagen guardada en galería", Toast.LENGTH_LONG).show()
     }
 }
