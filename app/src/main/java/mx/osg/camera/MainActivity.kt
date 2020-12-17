d722c004056139829c85b509bf9a08ae5e8711e5
ArcOscar <oscarperezgutierrez77@gmail.com>
2020-12-16 18:38:48 -0600

    Filtros de convolución y extras implementados. Pendiente la documentación y el desarrollo del filtro Zoom

--- app/src/main/java/mx/osg/camera/MainActivity.kt
+++ app/src/main/java/mx/osg/camera/MainActivity.kt
@@ -4,6 +4,7 @@ import `in`.goodiebag.carouselpicker.CarouselPicker
 import android.app.Activity
 import android.content.Intent
 import android.graphics.*
+import android.graphics.Color.*
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -63,8 +64,13 @@ class MainActivity : AppCompatActivity() {
         textItems.add(CarouselPicker.TextItem("Embossing", 9))   //11
         textItems.add(CarouselPicker.TextItem("Edge Detection", 9))   //12
         // Zoom
-        textItems.add(CarouselPicker.TextItem("Zoom", 9))   //13
+        textItems.add(CarouselPicker.TextItem("Zoom", 9))   //13    //FALTA JEJE
         // Extras
+        textItems.add(CarouselPicker.TextItem("Lossy", 9))   //14
+        textItems.add(CarouselPicker.TextItem("Stand", 9))   //15
+        textItems.add(CarouselPicker.TextItem("Sepia", 9))   //16
+        textItems.add(CarouselPicker.TextItem("Rotate", 9))   //17
+        textItems.add(CarouselPicker.TextItem("Pixels", 9))   //18
 
         val textAdapter = CarouselPicker.CarouselViewAdapter(this, textItems, 0)
         carouselPicker.adapter = textAdapter
@@ -100,6 +106,7 @@ class MainActivity : AppCompatActivity() {
         return File.createTempFile(fName, ".jpg", storageDirectory)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
@@ -153,19 +160,73 @@ class MainActivity : AppCompatActivity() {
                         } catch (e: Exception) {
                         }
                     }
-                    7 -> { /* TODO */
+                    7 -> {
+                        try {
+                            imageView.setImageBitmap(smooth(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
-                    8 -> { /* TODO */
+                    8 -> {
+                        try {
+                            imageView.setImageBitmap(gaussianBlur(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
-                    9 -> { /* TODO */
+                    9 -> {
+                        try {
+                            imageView.setImageBitmap(sharpen(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
-                    10 -> { /* TODO */
+                    10 -> {
+                        try {
+                            imageView.setImageBitmap(mean(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
-                    11 -> { /* TODO */
+                    11 -> {
+                        try {
+                            imageView.setImageBitmap(embossing(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
-                    12 -> { /* TODO */
+                    12 -> {
+                        try {
+                            imageView.setImageBitmap(edge(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
                     13 -> { /* TODO */
+                    }
+                    14 -> {
+                        try {
+                            imageView.setImageBitmap(lossy(imageTaken))
+                        } catch (e: Exception) {
+                        }
+                    }
+                    15 -> {
+                        try {
+                            imageView.setImageBitmap(stand(imageTaken))
+                        } catch (e: Exception) {
+                        }
+                    }
+                    16 -> {
+                        try {
+                            imageView.setImageBitmap(sepia(imageTaken))
+                        } catch (e: Exception) {
+                        }
+                    }
+                    17 -> {
+                        try {
+                            imageView.setImageBitmap(rotate(imageTaken))
+                        } catch (e: Exception) {
+                        }
+                    }
+                    18 -> {
+                        try {
+                            imageView.setImageBitmap(pixels(imageTaken))
+                        } catch (e: Exception) {
+                        }
                     }
                 }
             }
@@ -228,7 +289,7 @@ class MainActivity : AppCompatActivity() {
         // Apply gray filter to new bitmap
         canvas.drawBitmap(gray, 0F, 0F, paint)
 
-        // Return inverted Bitmap
+        // Return Bitmap
         return gray
     }
 
@@ -262,7 +323,7 @@ class MainActivity : AppCompatActivity() {
 
         bri.setPixels(pixels, 0, width, 0, 0, width, height)
 
-        // Return inverted Bitmap
+        // Return Bitmap
         return bri
     }
 
@@ -288,30 +349,30 @@ class MainActivity : AppCompatActivity() {
 
                 // get pixel color
                 pixel = cont.getPixel(x, y)
-                A = Color.alpha(pixel)
+                A = alpha(pixel)
 
                 // apply filter contrast for every channel R, G, B
-                R = Color.red(pixel)
+                R = red(pixel)
                 R = (((R / 255.0 - 0.5) * 2.5 + 0.5) * 255.0).toInt()
                 if (R < 0) R = 0
                 else if (R > 255) R = 255
 
-                G = Color.green(pixel)
+                G = green(pixel)
                 G = (((G / 255.0 - 0.5) * 2.5 + 0.5) * 255.0).toInt()
                 if (G < 0) G = 0
                 else if (G > 255) G = 255
 
-                B = Color.blue(pixel)
+                B = blue(pixel)
                 B = (((B / 255.0 - 0.5) * 2.5 + 0.5) * 255.0).toInt()
                 if (B < 0) B = 0
                 else if (B > 255) B = 255
 
                 // set new pixel color to output bitmap
-                cont.setPixel(x, y, Color.argb(A, R, G, B))
+                cont.setPixel(x, y, argb(A, R, G, B))
             }
         }
 
-        // Return inverted Bitmap
+        // Return Bitmap
         return cont
     }
 
@@ -332,18 +393,18 @@ class MainActivity : AppCompatActivity() {
         for (i in powers.indices) powers[i] =
             (Math.pow(i / 255.0f.toDouble(), 1.0f / gamma.toDouble()) * 255).toInt()
         for (p in pixels.indices) {
-            val r = Color.red(pixels[p])
-            val g = Color.green(pixels[p])
-            val b = Color.blue(pixels[p])
+            val r = red(pixels[p])
+            val g = green(pixels[p])
+            val b = blue(pixels[p])
             val newR = powers[r]
             val newG = powers[g]
             val newB = powers[b]
-            pixels[p] = Color.rgb(newR, newG, newB)
+            pixels[p] = rgb(newR, newG, newB)
         }
 
         gam.setPixels(pixels, 0, width, 0, 0, width, height)
 
-        // Return inverted Bitmap
+        // Return Bitmap
         return gam
     }
 
@@ -374,11 +435,381 @@ class MainActivity : AppCompatActivity() {
         for (i in 0 until pixels) pixel[i] = pixel[i] or color.toInt()
         rgb.setPixels(pixel, 0, width, 0, 0, width, height)
 
-        // Return inverted Bitmap
+        // Return Bitmap
         return rgb
     }
 
 // Convolution filters functions
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun convolution(
+        ori: Bitmap,
+        mod: Bitmap,
+        matrix: Array<Array<Int>>,
+        sumMatrix: Int,
+        extra: Int
+    ): Bitmap {
+        val w = ori.width
+        val h = ori.height
+
+        //apply the filter
+                var R1 = 0
+                var R2 = 0
+                var R3 = 0
+                var R4 = 0
+                var R5 = 0
+                var R6 = 0
+                var R7 = 0
+                var R8 = 0
+                var R9 = 0
+                var B1 = 0
+                var B2 = 0
+                var B3 = 0
+                var B4 = 0
+                var B5 = 0
+                var B6 = 0
+                var B7 = 0
+                var B8 = 0
+                var B9 = 0
+                var G1 = 0
+                var G2 = 0
+                var G3 = 0
+                var G4 = 0
+                var G5 = 0
+                var G6 = 0
+                var G7 = 0
+                var G8 = 0
+                var G9 = 0
+                var RP = 0
+                var GP = 0
+                var BP = 0
+
+                for (i in 2 until w - 2) {
+                    for (j in 2 until h - 2) {
+
+                        var pixelTL = ori.getPixel(i - 1, j - 1)
+                        R2 = Color.red(pixelTL) * matrix[0][0]
+                        G2 = Color.green(pixelTL) * matrix[0][0]
+                        B2 = Color.blue(pixelTL) * matrix[0][0]
+                        var pixelT = ori.getPixel(i, j - 1)
+                        R3 = Color.red(pixelT) * matrix[1][0]
+                        G3 = Color.green(pixelT) * matrix[1][0]
+                        B3 = Color.blue(pixelT) * matrix[1][0]
+                        var pixelTR = ori.getPixel(i + 1, j - 1)
+                        R4 = Color.red(pixelTR) * matrix[2][0]
+                        G4 = Color.green(pixelTR) * matrix[2][0]
+                        B4 = Color.blue(pixelTR) * matrix[2][0]
+
+                        var pixelML = ori.getPixel(i - 1, j)
+                        R1 = Color.red(pixelML) * matrix[0][1]
+                        G1 = Color.green(pixelML) * matrix[0][1]
+                        B1 = Color.blue(pixelML) * matrix[0][1]
+                        var pixeMT = ori.getPixel(i, j)
+                        R5 = Color.red(pixeMT) * matrix[1][1]
+                        G5 = Color.green(pixeMT) * matrix[1][1]
+                        B5 = Color.blue(pixeMT) * matrix[1][1]
+                        var pixelMR = ori.getPixel(i + 1, j)
+                        R9 = Color.red(pixelMR) * matrix[2][1]
+                        G9 = Color.green(pixelMR) * matrix[2][1]
+                        B9 = Color.blue(pixelMR) * matrix[2][1]
+
+                        var pixelBR = ori.getPixel(i + 1, j + 1)
+                        R6 = Color.red(pixelBR) * matrix[2][2]
+                        G6 = Color.green(pixelBR) * matrix[2][2]
+                        B6 = Color.blue(pixelBR) * matrix[2][2]
+                        var pixelB = ori.getPixel(i, j + 1)
+                        R7 = Color.red(pixelB) * matrix[1][2]
+                        G7 = Color.green(pixelB) * matrix[1][2]
+                        B7 = Color.blue(pixelB) * matrix[1][2]
+                        var pixelBL = ori.getPixel(i - 1, j + 1)
+                        R8 = Color.red(pixelBL) * matrix[0][2]
+                        G8 = Color.green(pixelBL) * matrix[0][2]
+                        B8 = Color.blue(pixelBL) * matrix[0][2]
+
+                        RP = (R1 + R2 + R3 + R4 + R5 + R6 + R7 + R8 + R9) / sumMatrix + extra
+                        GP = (G1 + G2 + G3 + G4 + G5 + G6 + G7 + G8 + G9) / sumMatrix + extra
+                        BP = (B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9) / sumMatrix + extra
+
+                        mod.setPixel(i, j, argb(255, RP, GP, BP))
+                    }
+                }
+
+        // return new bitmap
+        return mod
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun smooth(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(1, 1, 1),
+            arrayOf(1, 1, 1),
+            arrayOf(1, 1, 1)
+        )
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gSmo = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gSmo2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 1
+        var extra = 9
+
+        // Return Bitmap
+        return convolution(gSmo, gSmo2, kernel, sum, extra)
+    }
+
+    // Apply gaussianBlur effect on the bitmap
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun gaussianBlur(original: Bitmap): Bitmap? {
+        val gauss = arrayOf(
+            arrayOf(1, 2, 1),
+            arrayOf(2, 4, 2),
+            arrayOf(1, 2, 1)
+        )
+
+        var sum = 0
+        var extra = 0
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gGauss = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gGauss2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        for (y in 0 until 3)
+            for (x in 0 until 3)
+                sum += gauss[x][y]
+
+        // Return Bitmap
+        return convolution(gGauss, gGauss2, gauss, sum, extra)
+    }
+
+    // Apply sharpen effect on the bitmap
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun sharpen(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(0, -2, 0),
+            arrayOf(-2, 11, -2),
+            arrayOf(0, -2, 0)
+        )
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gSharp = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gSharp2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 0
+        var extra = 0
+
+        for (y in 0 until 3)
+            for (x in 0 until 3)
+                sum += kernel[x][y]
+
+        // Return Bitmap
+        return convolution(gSharp, gSharp2, kernel, sum, extra)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun mean(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(-1, -1, -1),
+            arrayOf(-1, 9, -1),
+            arrayOf(-1, -1, -1)
+        )
+
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gMean = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gMean2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 0
+        var extra = 0
+
+        for (y in 0 until 3)
+            for (x in 0 until 3)
+                sum += kernel[x][y]
+
+        // Return Bitmap
+        return convolution(gMean, gMean2, kernel, sum, extra)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun embossing(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(-1, 0, -1),
+            arrayOf(0, 4, 0),
+            arrayOf(-1, 0, -1)
+        )
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gEmb = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gEmb2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 1
+        var extra = 127
+
+        // Return Bitmap
+        return convolution(gEmb, gEmb2, kernel, sum, extra)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun edge(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(1, 1, 1),
+            arrayOf(0, 0, 0),
+            arrayOf(-1, -1, -1)
+        )
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gEdg = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gEdg2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 1
+        var extra = 127
+
+        // Return Bitmap
+        return convolution(gEdg, gEdg2, kernel, sum, extra)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun lossy(original: Bitmap): Bitmap? {
+        val kernel= arrayOf(
+            arrayOf(1, -2, 1),
+            arrayOf(-2, 4, -2),
+            arrayOf(-2, 1, -2)
+        )
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val gEmb = original.copy(Bitmap.Config.ARGB_8888, true)
+        val gEmb2 = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        var sum = 1
+        var extra = 70
+
+        // Return Bitmap
+        return convolution(gEmb, gEmb2, kernel, sum, extra)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun stand(original: Bitmap): Bitmap? {
+
+        val width = original.getWidth()
+        val height = original.getHeight()
+        val bmOut = Bitmap.createBitmap(width, height, original.getConfig())
+        var A:Int
+        var R:Int
+        var G:Int
+        var B:Int
+        var pixel:Int
+
+        for (x in 0 until width)
+        {
+            for (y in 0 until height)
+            {
+                // get pixel color
+                pixel = original.getPixel(x, y)
+                // apply filtering on each channel R, G, B
+                A = Color.alpha(pixel)
+                R = (Color.red(pixel) * 5)
+                G = (Color.green(pixel) * 2)
+                B = (Color.blue(pixel) * 2)
+                // set new color pixel to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B))
+            }
+        }
+
+        return bmOut
+    }
+
+    fun sepia(src: Bitmap): Bitmap? {
+        // image size
+        var src = src
+        val width = src!!.width
+        val height = src.height
+        // create output bitmap
+        val bmOut = Bitmap.createBitmap(width, height, src.config)
+        // constant grayscale
+        val GS_RED = 0.3
+        val GS_GREEN = 0.59
+        val GS_BLUE = 0.11
+        // color information
+        var A: Int
+        var R: Int
+        var G: Int
+        var B: Int
+        var pixel: Int
+
+        // scan through all pixels
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                // get pixel color
+                pixel = src.getPixel(x, y)
+                // get color on each channel
+                A = alpha(pixel)
+                R = red(pixel)
+                G = green(pixel)
+                B = blue(pixel)
+                // apply grayscale sample
+                R = (GS_RED * R + GS_GREEN * G + GS_BLUE * B).toInt()
+                G = R
+                B = G
+
+                // apply intensity level for sepid-toning on each channel
+                R += 110
+                if (R > 255) {
+                    R = 255
+                }
+                G += 65
+                if (G > 255) {
+                    G = 255
+                }
+                B += 20
+                if (B > 255) {
+                    B = 255
+                }
+
+                // set new pixel color to output image
+                bmOut.setPixel(x, y, argb(A, R, G, B))
+            }
+        }
+
+        return bmOut
+    }
+
+    fun rotate(source: Bitmap): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(90F)
+        val rot = source.copy(Bitmap.Config.ARGB_8888, true)
+        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
+    }
+
+    fun pixels(original: Bitmap): Bitmap? {
+
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        val pix = original.copy(Bitmap.Config.ARGB_8888, true)
+
+        // Get info about Bitmap
+        val width = pix.width
+        val height = pix.height
+
+        val tamPix = 80
+        var contPix = 0
+        var R = 0
+        var B = 0
+        var G = 0
+
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                if (contPix == tamPix || x == 0) {
+                    R = red(pix.getPixel(x, y))
+                    G = red(pix.getPixel(x, y))
+                    B = red(pix.getPixel(x, y))
+                    contPix = 0
+                }
+                pix.setPixel(x, y, rgb(R, G, B))
+                contPix++
+            }
+        }
+
+        // Return inverted Bitmap
+        return pix
+    }
 
     // Saving the photo into the gallery
     private fun saveToGallery() {
